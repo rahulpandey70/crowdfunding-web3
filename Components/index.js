930a374e7aa46ddd4c153e4eead4a0a3a5a109fe
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-07 00:34:06 +0530

    Update index file

--- Components/index.js
+++ Components/index.js
@@ -2,5 +2,8 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Logo from "./Logo";
 import Menu from "./Menu";
+import Card from "./Card";
+import Hero from "./Hero";
+import PopUp from "./PopUp";
 
-export { Navbar, Footer, Logo, Menu };
+export { Navbar, Footer, Logo, Menu, Card, Hero, PopUp };
